fbac4e07a517aae77cecac09e2082d9a2460a5da
asegal92 <anastasiia.segal@gmail.com>
2023-02-25 08:48:39 -0500

    .env corrected

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 app.use(express.static('public'));
 app.use(methodOverride('_method'))
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
